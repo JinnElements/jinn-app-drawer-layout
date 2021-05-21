00f05cd47f2868fbcfb2d5213d8565d53eee1126
JoernT <joern.turner@gmail.com>
2021-05-21 18:01:12 +0200

    first custom version

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ A simple drawer layout in pure css
 ## Note for production use
 
 pure css drawer is quite huge. A custom build should be made when using in production.
+
+see - https://github.com/mac81/pure-drawer
--- src/JinnAppDrawerLayout.js
+++ src/JinnAppDrawerLayout.js
@@ -16,11 +16,11 @@ export class JinnAppDrawerLayout extends HTMLElement {
 
   connectedCallback(){
     const style = `
-        @import "./src/pure-drawer.min.css";
+        @import "./src/pure-min.min.css";
         .pure-toggle-label{
           border:none;
-          width:45px;
-          height:45px;
+          // width:45px;
+          // height:45px;
         }
         .pure-toggle-label .pure-toggle-icon, .pure-toggle-label .pure-toggle-icon::before, .pure-toggle-label .pure-toggle-icon::after{
           background:var(--jinn-drawer-icon);
@@ -29,25 +29,51 @@ export class JinnAppDrawerLayout extends HTMLElement {
           background:var(--jinn-drawer-icon-hover);
           color:var(--jinn-drawer-icon);
         }
+        .visible{
+          visibility:visible;
+          opacity:1;
+          transition: opacity 1s easeIn;
+        }
+        .hidden{
+          visibility:hidden;
+          opacity:0;
+          transition:opacity 1s easeOut;
+        }
     `;
+
     this.shadowRoot.innerHTML = `
-      <style>
-        ${style}
-      </style>
+        <style>
+          ${style}
+        </style>
       ${this.render()}
     `;
 
-    this.effect = this.hasAttribute('effect')?this.getAttribute('effect'):'pure-pusher'
+    window.addEventListener('load', ()=>{
+      console.log('loaded');
+      const icon = this.shadowRoot.querySelector('.pure-toggle-label');
+      // const drawer = this.shadowRoot.querySelector('.pure-drawer');
+      // drawer.style.visibility='visible';
+      // document.body.style.visibility = 'visible';
+      document.body.style.visibility = 'visible';
+
+      setTimeout(()=>{
+        // icon.style.visibility='visible';
+        const hidden = this.shadowRoot.querySelector('.hidden');
+        hidden.classList.replace('hidden', 'visible');
+      },400);
+    });
+
+    // this.effect = this.hasAttribute('effect')?this.getAttribute('effect'):'pure-pusher'
   }
 
 
   render() {
     return `
         <div class="pure-container" data-effect="pure-effect-scaleRotate">
-            <input type="checkbox" id="pure-toggle-left" class="pure-toggle" data-toggle="left"/>
-            <label class="pure-toggle-label" for="pure-toggle-left" data-toggle-label="left"><span class="pure-toggle-icon"></span></label>
+            <input type="checkbox" id= "pure-toggle-left" class="pure-toggle" data-toggle="left"/>
+            <label class="pure-toggle-label hidden" for="pure-toggle-left" data-toggle-label="left"><span class="pure-toggle-icon"></span></label>
     
-            <nav class="pure-drawer" data-position="left">
+            <nav  class="pure-drawer" data-position="left">
                 <p style="padding: 3rem 0; margin: 0;">
                     <slot name="drawer"></slot>
                 </p>
